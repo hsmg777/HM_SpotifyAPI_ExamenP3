0a70206698d204d8a313e86ded6718d6abd45ac0
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2024-01-24 08:13:36 -0500

    Creado SQLlite + repository con metodos

--- HM_SpotifyAPI_ExamenP3/Repositories/guardadoRepository.cs
+++ HM_SpotifyAPI_ExamenP3/Repositories/guardadoRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HM_SpotifyAPI_ExamenP3.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,72 @@ using System.Threading.Tasks;
 
 namespace HM_SpotifyAPI_ExamenP3.Repositories
 {
-    internal class guardadoRepository
+    public class guardadoRepository
     {
+        SQLiteConnection connection;
+        public string StatusMessagge { get; set; }
+        public guardadoRepository()
+        {
+            connection =
+                new SQLiteConnection(Constants.DatabasePath,
+                Constants.Flags);
+            connection.CreateTable<guardados>();
+
+        }
+        public void Add(guardados save)
+        {
+            int result = 0;
+            try
+            {
+                    result = connection.Insert(save);
+                    StatusMessagge =
+                        $"{result} row(s) addded";
+
+            }
+            catch (Exception ex)
+            {
+                StatusMessagge = $"ERROR {ex.Message}";
+
+            }
+
+        }
+        public List<guardados> GetAll()
+        {
+            try
+            {
+                return connection.Table<guardados>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessagge = $"ERROR {ex.Message}";
+            }
+            return null;
+        }
+        public guardados Get(int id)
+        {
+            try
+            {
+                return connection.Table<guardados>().FirstOrDefault(x => x.id == id);
+            }
+            catch (Exception ex)
+            {
+                StatusMessagge = $"ERROR {ex.Message}";
+            }
+            return null;
+        }
+
+        public void Delete(int saveId)
+        {
+            try
+            {
+                var save =
+                    Get(saveId);
+                connection.Delete(save);
+            }
+            catch (Exception ex)
+            {
+                StatusMessagge = $"ERROR {ex.Message}";
+            }
+        }
     }
 }
